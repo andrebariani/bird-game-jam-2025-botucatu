ef23a8a3b2c93477dc1236d7085bc677807befd4
André Trava <andre.trava_contractor@celanese.com>
2025-03-30 00:34:27 -0300

    start fix

--- src/player/states/idle.gd
+++ src/player/states/idle.gd
@@ -3,5 +3,9 @@ extends State
 func run(_delta):
 	var e: PlayerBigua = entity
 	
-	if e.inputs.dirv:
-		end("Move")
+	if e.run and $StartTimer.is_stopped():
+		$StartTimer.start()
+
+func _on_start_timer_timeout():
+	print_debug('help')
+	end("Move")
